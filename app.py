389210462c90292ed480417db7f0915a720d4fb9
frenziedcode <contact.viniciuslopes@gmail.com>
2024-06-06 19:33:03 +0100

    "update"

--- app.py
+++ app.py
@@ -241,4 +241,4 @@ def update_rate():
 # ----------------- Inicializa a aplicação no server
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
